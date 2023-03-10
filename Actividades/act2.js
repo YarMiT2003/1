1465cc94492dfa9123ad7250b1e155a1f2c35334
Yaritza Tun <210349@mail.unacar.mx>
2023-03-10 14:32:14 -0600

    Actividad2

--- Actividades/act2.js
+++ Actividades/act2.js
@@ -1,7 +1,7 @@
 var temp = prompt("¿Cuál es la temperatura en grados Fahrenheit?")
 console.log(temp);
 sum = (temp-32)*(0.5556)
-alert("La temperatura es:")
+alert("La temperatura en grados Celsius es:")
 console.log(sum);
 alert(sum)
 
