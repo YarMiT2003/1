2f2a03568251903727422cd768032ff702d2a9a0
Yaritza Tun <210349@mail.unacar.mx>
2023-03-11 02:25:33 -0600

    Acti3

--- Actividades/act2.js
+++ Actividades/act2.js
@@ -1,6 +1,6 @@
 var temp = prompt("¿Cuál es la temperatura en grados Fahrenheit?")
 console.log(temp);
-sum = (temp-32)*(0.5556)
+var sum = (temp-32)*(0.5556)
 alert("La temperatura en grados Celsius es:")
 console.log(sum);
 alert(sum)
--- Actividades/act3.js
+++ Actividades/act3.js
@@ -4,3 +4,23 @@ var v2 = prompt("Dame el valor 2")
 console.log(v2)
 var v3 = prompt("Dame el valor 3")
 console.log(v3)
+
+if(v1 > v2)
+{
+t = n1;
+}
+    else
+    {
+        t = v2;
+    }
+
+if (t > v3)
+{
+    mayor = t;
+}
+    else
+    {
+        mayor = v3;
+    }
+alert('El Valor Mayor es:')
+alert(mayor)
